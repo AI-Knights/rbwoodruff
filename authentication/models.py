05dd016b433ab6953f60fdbbaf793f42aa887427
jamil <jamilhossain3251@gmail.com>
2025-11-12 02:56:21 +0600

    Your commit message here

--- authentication/models.py
+++ authentication/models.py
@@ -24,11 +24,12 @@ class CustomAccountManager(BaseUserManager):
         extra_fields.setdefault("is_active", True)
 
         if extra_fields.get("is_staff") is not True:
-            raise ValueError(_("Superuser must have is_staff=True."))
+            raise ValueError("Superuser must have is_staff=True.")
         if extra_fields.get("is_superuser") is not True:
-            raise ValueError(_("Superuser must have is_superuser=True."))
+            raise ValueError("Superuser must have is_superuser=True.")
         if extra_fields.get("role") != "admin":
-            raise ValueError(_("Superuser must have role='admin'."))
+            raise ValueError("Superuser must have role='admin'.")
+        
         return self.create_user(email, password, **extra_fields)
     
 
@@ -83,7 +84,7 @@ class OTP(models.Model):
     user = models.ForeignKey('UserAccount', on_delete=models.CASCADE, related_name='otps')
     otp = models.CharField(max_length=6)
     created_at = models.DateTimeField(auto_now_add=True)
-
+    
     def is_valid(self):
         expiry_duration = getattr(settings, 'OTP_VALIDITY_DURATION', 5)  # minutes
         return timezone.now() <= self.created_at + timezone.timedelta(minutes=expiry_duration)
@@ -97,3 +98,4 @@ class OTP(models.Model):
         indexes = [
             models.Index(fields=['created_at']),  # speeds up deletion queries
         ]
+
