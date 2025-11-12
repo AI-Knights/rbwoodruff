c39c2e5947416cd666f8a1a41da7a7897fd17fed
jamil <jamilhossain3251@gmail.com>
2025-11-12 06:55:02 +0600

    update

--- authentication/views.py
+++ authentication/views.py
@@ -1,9 +1,12 @@
-from rest_framework import APIView
+from rest_framework import APIView, generics
+from .serializers import RegisterSerializer
+from rest_framework.permissions import AllowAny
 
 
 
 
+class RegisterView(generics.CreateAPIView):
+    serializer_class = RegisterSerializer
+    authentication_classes = AllowAny
 
-
-class RegisterView(APIView):
     
